a38c0aacf478fa3c2722a9232bce9cbd666c1d67
Serhii Korniichuk <serhii.korniichuk.dev@gmail.com>
2023-08-11 18:29:03 +0300

    feat: create Typography

---
+++ src/components/Typography/types.ts
@@ -0,0 +1,31 @@
+export type TypographyTag =
+  | 'h1'
+  | 'h2'
+  | 'h3'
+  | 'h4'
+  | 'h5'
+  | 'h6'
+  | 'p'
+  | 'span'
+  | 'button'
+  | 'sup'
+  | 'sub'
+
+export type TypographyVariant =
+  | 'h1'
+  | 'h2'
+  | 'h4'
+  | 'h5'
+  | 'subtitle1'
+  | 'body1'
+  | 'body1-bold'
+  | 'body2'
+  | 'button1'
+  | 'button2'
+  | 'price1'
+  | 'price2'
+  | 'price3'
+
+export type TypographyVariantsDefinition = Record<TypographyVariant, string>
+
+export type TypographyAlign = 'inherit' | 'left' | 'center' | 'right'
